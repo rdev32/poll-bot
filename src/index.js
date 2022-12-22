553330a2a0619ac847942a86037c0e5b59eadea0
rdev32 <rdevelops32@gmail.com>
2022-12-22 00:49:14 -0500

    update v1.2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import 'dotenv/config'
-import { 
-    Client, 
-    GatewayIntentBits, 
-    EmbedBuilder, 
-    ActionRowBuilder, 
+import {
+    Client,
+    GatewayIntentBits,
+    EmbedBuilder,
+    ActionRowBuilder,
     ButtonBuilder,
     ButtonStyle
 } from 'discord.js'
@@ -18,6 +18,30 @@ const client = new Client({
         GatewayIntentBits.MessageContent
     ]
 })
+
+const locales = {
+    'es-ES': 'Ya votaste'
+}
+
+class Session {
+    constructor(messageID) {
+        this.sessionID = messageID
+        this.sessionUsers = new Array()
+    }
+}
+
+class SessionManager extends Session {
+    constructor() {
+        super()
+    }
+
+    joinSession() {
+        for (let i = 0; i < sessions.length; i++) {
+            if (sessions[i].id == messageId)
+                sessions[i].users.push(userId)
+        }   
+    }
+}
 
 const sessions = []
 
@@ -35,11 +59,12 @@ function joinSession(messageId, userId) {
     }
 }
 
-function alreadyVoted(messageId, userId) {
+function checkInSession(messageId, userId) {
     for (let i = 0; i < sessions.length; i++) {
         if (sessions[i].id == messageId)
-            return sessions[i].users.includes(userId) ? true : false
+            return sessions[i].users.includes(userId)
     }
+    return false
 }
 
 client.once('ready', () => {
@@ -48,7 +73,6 @@ client.once('ready', () => {
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isButton()) return
-    
     const interactionButton = interaction.customId
     const interactionEmbedMessage = interaction.message.embeds.pop()
     const interactionEdit = new EmbedBuilder()
@@ -57,58 +81,61 @@ client.on('interactionCreate', async interaction => {
         .setDescription(interactionEmbedMessage.description)
 
     let interactionFields = interactionEmbedMessage.fields
-    
-    if (sessions.length > 0) {
-        if (alreadyVoted(interaction.message.id, interaction.user.id))
-            return interaction.reply({
-                content: 'Ya votaste',
-                ephemeral: true
-            })
+    let responseValid = true
+    if (checkInSession(interaction.message.id, interaction.user.id)) {
+        responseValid = false
     }
+    if (responseValid) {
+        switch (interactionButton) {
+            case 'btn-0':
+                joinSession(interaction.message.id, interaction.user.id)
+                interactionFields[0].value = (parseInt(interactionFields[0].value) + 1).toString()
+                interactionEdit.addFields(interactionFields)
+                break
 
-    switch (interactionButton) {
-        case 'btn-0':
-            joinSession(interaction.message.id, interaction.user.id)           
-            interactionFields[0].value = (parseInt(interactionFields[0].value) + 1).toString()
-            interactionEdit.addFields(interactionFields)
+            case 'btn-1':
+                joinSession(interaction.message.id, interaction.user.id)
+                interactionFields[1].value = (parseInt(interactionFields[1].value) + 1).toString()
+                interactionEdit.addFields(interactionFields)
+                break
 
-            break
-            
-        case 'btn-1':
-            joinSession(interaction.message.id, interaction.user.id)
-            interactionFields[1].value = (parseInt(interactionFields[1].value) + 1).toString()
-            interactionEdit.addFields(interactionFields)
-            break
+            case 'btn-2':
+                joinSession(interaction.message.id, interaction.user.id)
+                interactionFields[2].value = (parseInt(interactionFields[2].value) + 1).toString()
+                interactionEdit.addFields(interactionFields)
+                break
+                break
 
-        case 'btn-2':
-            joinSession(interaction.message.id, interaction.user.id)
-            interactionFields[2].value = (parseInt(interactionFields[2].value) + 1).toString()
-            interactionEdit.addFields(interactionFields)
-            break
+            case 'btn-3':
+                joinSession(interaction.message.id, interaction.user.id)
+                interactionFields[3].value = (parseInt(interactionFields[3].value) + 1).toString()
+                interactionEdit.addFields(interactionFields)
+                break
 
-        case 'btn-3':
-            joinSession(interaction.message.id, interaction.user.id)
-            interactionFields[3].value = (parseInt(interactionFields[3].value) + 1).toString()
-            interactionEdit.addFields(interactionFields)
-            break
+            default:
+                break
+        }
 
-        default:
-            break
+        await interaction.update({ embeds: [interactionEdit] })
+    } else {
+
+        await interaction.reply({
+            content: locales[interaction.locale] ?? 'You already voted!',
+            ephemeral: true
+        })
     }
-
-    await interaction.update({ embeds: [interactionEdit] })
 })
 
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return
+    if (!interaction.isChatInputCommand()) return
 
-	const { commandName } = interaction
+    const { commandName } = interaction
 
-	if (commandName === 'poll') {
+    if (commandName === 'poll') {
         const totalArg = parseInt(interaction.options.getString('total'))
         const numberOfButtons = (totalArg < 6 && totalArg > 1) ? totalArg : 2
         const optionsArgs = []
-        
+
         for (let i = 0; i < numberOfButtons; i++)
             optionsArgs.push(interaction.options.getString(`option${i}`))
 
@@ -119,19 +146,19 @@ client.on('interactionCreate', async interaction => {
                 .setCustomId(`btn-${i}`)
                 .setLabel(`${optionsArgs[i]}`)
                 .setStyle(ButtonStyle.Secondary))
-            
+
             embedResults.push({ name: `${optionsArgs[i]}`, value: '0', inline: true })
         }
-        
+
         const optionsRow = new ActionRowBuilder().addComponents(buttons)
-        
+
         const pollMessage = new EmbedBuilder()
             .setColor(0xf5dd42)
             .setTitle(interaction.options.getString('question'))
             .setDescription(`Poll made by ${interaction.user.tag}`)
             .addFields(embedResults)
 
-		const { id } = await interaction.reply({ embeds: [pollMessage], components: [optionsRow], fetchReply: true})
+        const { id } = await interaction.reply({ embeds: [pollMessage], components: [optionsRow], fetchReply: true })
         createSession(id)
     }
 })
