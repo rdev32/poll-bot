4e3e75f4f89d2c2b02b76aac83cf4982d63a8253
rdev32 <rdevelops32@gmail.com>
2022-12-24 12:23:10 -0500

    version 2.1

--- src/commands.js
+++ src/commands.js
@@ -25,9 +25,9 @@ let commands = [
 				.setDescription('How much time will your poll last?')
 				.setRequired(true)
 				.addChoices(
-					{ name: '5 seconds', value: '5000' },
-					{ name: '15 minutes', value: '900000'},
-					{ name: '30 minutes', value: '1800000'},
+					{ name: '5 minutes', value: '300000' },
+					{ name: '15 minutes', value: '900000' },
+					{ name: '30 minutes', value: '1800000' },
 					{ name: '1 hour', value: '3600000' },
 					{ name: '2 hours', value: '7200000' },
 				)
